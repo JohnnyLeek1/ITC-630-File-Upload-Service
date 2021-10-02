157fd37638a699328161408b684ceaf620bd62d4
Colin Truemner <cjaytruemner@gmail.com>
2021-10-02 02:02:49 +0000

    fixed upload view to upload file and not filename

--- upload_site/upload/views.py
+++ upload_site/upload/views.py
@@ -9,18 +9,18 @@ def index(request):
     context = { 'test': 'johnny' }
     return HttpResponse(template.render(context, request))
 
-def upload_file(source_file_name, destination_blob_name):
+def upload_file(upload_file, destination_blob_name):
     """Uploads file to bucket"""
-    # source_file_name is the path to the file to upload
+    # upload_file is the file to upload
     # destination_blob_name is the id of GCS object
 
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
 
-    blob.upload_form_filename(source_file_name)
+    blob.upload_from_file(upload_file)
 
-    print("File {} uploaded to {}".format(source_file_name, destination_blob_name))
+    print("File {} uploaded to {}".format(str(upload_file), destination_blob_name))
 
 def download_blob(source_blob_name, destination_file_name):
     """Downloads a blob from the bucket"""
